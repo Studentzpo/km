cac85ffe24062721c281bd17ad1ee2342d339f2d
choatchaw <choatchaw@Choatchaws-MacBook-Pro.local>
2017-10-27 14:34:35 +0700

    แก้ไข

--- web/index.php
+++ web/index.php
@@ -3,57 +3,62 @@
 	<head>
 		<title> ::HELLO:: </title>
         <meta charset="utf-8"> 
-        <link rel="stylesheet" href="../css/bootstrap.min.css">
-        <img src="a.jpg" height="250px">
-        
+        <link rel="stylesheet" href="../css/bootstrap.min.css">  
 	</head>
-    <body>
-    	<div class="container">
-  	<div class="row">
-    	<div class="col-md-12">
-       	 	<div class="col-md-4" style="float: left; border:1px solid black height="200px" width="200px">
-            	<top><center>Fury</top></center>
-            		<center><img src="1.png" height="200px" width="200px"</center>
-                    	<bottom><center>อ่านรายละเอียด...</bottom></center> 
+<body>
+<div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="a.jpg" style="width: 100%">
+                </div>
+            </div>
+        	
+      	<div class="row">
+        	<div class="col-md-12">
+           	 	<div class="col-md-4" style="float: left; border:1px solid black; height:250px; width:220px">
+                	<top><center>Fury</top></center>
+                    <center><img src="1.png" height="200px" width="200px"></center>
+                    <bottom><center>อ่านรายละเอียด...</center></bottom>
+                </div>
+            	<div class="col-md-4" style="float: left; border:1px solid black; height:250px; width:220px">
+                	<top><center>Hope</top></center>
+                		<center><img src="2.png" height="200px" width="200px"></center>
+                        	<bottom><center>อ่านรายละเอียด...</bottom></center> 
+                </div>
+            	<div class="col-md-4" style="float: left; border:1px solid black; height:250px; width:220px">
+                	<top><center>Peace</top></center>
+                		<center><img src="3.png" height="200px" width="200px"></center>
+                        	<bottom><center>อ่านรายละเอียด...</bottom></center>   		
+                </div>
+          </div>   
+      </div>
+      
+      <div class="row">
+        	<div class="col-md-12">
+           	 	<div class="col-md-4" style="float: left; border:1px solid black; height:200px; width:200px">
+                	<top><center>Fury</top></center>
+                		<center><img src="1.png" height="200px" width="200px"></center>
+                        	<bottom><center>อ่านรายละเอียด...</bottom></center> 
+                </div>
+    
+            	<div class="col-md-4" style="float: left; border:1px solid black; height:200px; width:200px">
+                	<top><center>Hope</top></center>
+                		<center><img src="2.png" height="200px" width="200px"></center>
+                        	<bottom><center>อ่านรายละเอียด...</bottom></center> 
+                </div>
+            	<div class="col-md-4" style="float: left; border:1px solid black; height:200px; width:200px">
+                	<top><center>Peace</top></center>
+                		<center><img src="3.png" height="200px" width="200px"></center>
+                        	<bottom><center>อ่านรายละเอียด...</bottom></center>   		
+                </div>
+            </div>   
         </div>
-        		
-        	<div class="col-md-4" style="float: left; border:1px solid black height="200px" width="200px">
-            	<top><center>Hope</top></center>
-            		<center><img src="2.png" height="200px" width="200px"</center>
-                    	<bottom><center>อ่านรายละเอียด...</bottom></center> 
-        </div>
-        	<div class="col-md-4" style="float: left; border:1px solid black height="200px" width="200px">
-            	<top><center>Peace</top></center>
-            		<center><img src="3.png" height="200px" width="200px"</center>
-                    	<bottom><center>อ่านรายละเอียด...</bottom></center>   		
-        </div>
-      </div>   
-  </div>
-  <div class="row">
-    	<div class="col-md-12">
-       	 	<div class="col-md-4" style="float: left; border:1px solid black height="200px" width="200px">
-            	<top><center>Fury</top></center>
-            		<center><img src="1.png" height="200px" width="200px"</center>
-                    	<bottom><center>อ่านรายละเอียด...</bottom></center> 
-        </div>
-        		
-        	<div class="col-md-4" style="float: left; border:1px solid black height="200px" width="200px">
-            	<top><center>Hope</top></center>
-            		<center><img src="2.png" height="200px" width="200px"</center>
-                    	<bottom><center>อ่านรายละเอียด...</bottom></center> 
-        </div>
-        	<div class="col-md-4" style="float: left; border:1px solid black height="200px" width="200px">
-            	<top><center>Peace</top></center>
-            		<center><img src="3.png" height="200px" width="200px"</center>
-                    	<bottom><center>อ่านรายละเอียด...</bottom></center>   		
-        </div>
-      </div>   
-  </div>
-  
-    </body>
+</div>
+
     <footer>
     	BY:CHOTINUN PHANPECH
     </footer>
+</body>
 </html>
 
     

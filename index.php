55e871ee4c371cfee70cfe9c4897d1a8a892d4cb
chotinun <32922419+chotinun@users.noreply.github.com>
2017-10-31 21:34:37 +0700

    กล่องสามสี
    ทำการสร้างกล่องสามกล่องกำหนดสี

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <html>
     <head>
     	<meta charset="utf-8">
-        <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/main.css">
     </head>
-    <body class="wrap">
-   
-        
-    </body>
+    <body class="blockContainer">
+        	<div class="left"></div>
+            <div class="Center"></div>
+            <div class="right"></div>
+        </div>
+   	</body>
 </html>

2c63fa780a7ee6e91acdd362e282f86e1c96ed2c
chotinun <32922419+chotinun@users.noreply.github.com>
2017-11-01 13:34:31 +0700

    กล่องสามสี(แก้ไข)
    ลบwidth ออก

--- index.php
+++ index.php
@@ -2,12 +2,24 @@
 <html>
     <head>
     	<meta charset="utf-8">
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
         <link rel="stylesheet" href="css/main.css">
     </head>
-    <body class="blockContainer">
-        	<div class="left"></div>
-            <div class="Center"></div>
-            <div class="right"></div>
-        </div>
+    <body>
+    	<div class="container">
+        <div class="blockContainer">
+            <div class="row">
+                <div class="col-md-12">
+                <div class="col-md-4">
+        			<div class="left"></div>
+            </div>
+                <div class="col-md-4">
+            		<div class="Center"></div>
+            </div>
+           		<div class="col-md-4">
+            		<div class="right"></div>
+               </div>
+        	</div>
+      </div>
    	</body>
 </html>

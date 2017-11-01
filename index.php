97d78fb35d43b9a170f46d9ab4e59344cecc8a1a
chotinun <32922419+chotinun@users.noreply.github.com>
2017-11-01 17:49:20 +0700

    กล่อง

--- index.php
+++ index.php
@@ -6,20 +6,18 @@
         <link rel="stylesheet" href="css/main.css">
     </head>
     <body>
-    	<div class="container">
-        <div class="blockContainer">
-            <div class="row">
-                <div class="col-md-12">
-                <div class="col-md-4">
-        			<div class="left"></div>
-            </div>
-                <div class="col-md-4">
-            		<div class="Center"></div>
-            </div>
-           		<div class="col-md-4">
-            		<div class="right"></div>
-               </div>
-        	</div>
-      </div>
-   	</body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+ 				<div class="col-md-4 news">
+            	<p style="padding:40px;">test1</p>
+                </div> 
+            	<div class="col-md-4"style="float:left; border:1px solid black; background:#EE82EE; height:250px; width:220px">
+            	</div>
+            	<div class="col-md-4"style="float:left; border:1px solid black; background:#DB7093; height:250px; width:220px">
+				</div>
+			</div>
+    	</div>
+	</div>
+    </body>
 </html>
---
+++ work/test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8">
+    <title> </title>
+		<link rel="stylesheet" href="../../css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="/css/codecss.css">
+	</head>
+<body>
+	<div class="container">
+    
+    
+<footer>
+	Contact Us
+</footer>
+	</div>	
+</body>
+</html>

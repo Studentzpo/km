f0d4ef0ddc83334061573689c7c3b7222c1d9b85
chotinun <32922419+chotinun@users.noreply.github.com>
2017-11-14 21:28:14 +0700

    หน้าวิดีโอ
    ทำการใส่วิดีโอที่ลิงก์มาจากยูทูป

---
+++ Video.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <script src="js/jquery-3.2.1.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+            <img src="images/km/bannervideo.jpg" width="100%"><!----แบนเนอร์--->
+            </div>
+        </div>
+       	<div class="row">
+       		<div class="col-md-12"> <!---เลือกประเภท--->
+            	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  					<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    					<div class="navbar-nav">
+                             <a class="nav-item nav-link active" href="VideoHardwere.php">ฮาร์ดแวร์</a>
+                             <a class="nav-item nav-link active" href="VideoSoftwere.php">ซอฟแวร์</a>
+                             <a class="nav-item nav-link active" href="VideoNetwork.php">ระบบเครือข่าย</a>
+                             <a class="nav-item nav-link active" href="VideoOffice.php">อุปกรณ์สำนักงาน</a>
+                             <a class="nav-item nav-link active" href="VideoDoctor.php">โภชนาการอาหารสัตว์</a>
+                             <a class="nav-item nav-link active" href="VideoFood.php">การรักษาสัตว์</a>
+            			</div>
+        			</div>
+    			</nav>
+			</div>
+		</div>
+		<div class="row">
+        	<div class="col-md-12">
+            	<h3>Video</h3>
+            </div>
+       	</div>
+       	<div class="row">
+       		<div class="col-md-12">
+        		<div class="col-md-4" style="float:left;">
+                	<a data-fancybox href="https://www.youtube.com/watch?v=l8PED2YtpmA&list=PL2MW50dbNlbYWFq9AycOoAvtQkR_zFNx2">
+      					<img src="images/km/ots_t.jpg" width="320" height="240"></a>
+                        	<h3>Office the Series Trailer</h3>
+     			</div>
+                <div class="col-md-4" style="float:left;">
+                	<a data-fancybox href=			"https://www.youtube.com/watch?v=f4BisN2fyHs&list=PL2MW50dbNlbYWFq9AycOoAvtQkR_zFNx2&index=2">
+      					<img src="images/km/ots_1.jpg" width="320" height="240"></a>
+                        	<h3>Office the Series ตอนที่ 1 พ.ร.บ.คอมพิวเตอร์ 2550</h3>
+     			</div>
+               <div class="col-md-4" style="float:left;">
+                	<a data-fancybox href="https://www.youtube.com/watch?v=d-Bgu2L9R8U&index=13&list=PL2MW50dbNlbYWFq9AycOoAvtQkR_zFNx2">
+      					<img src="images/km/ots_2.jpg" width="320" height="240"></a>
+                        	<h3>Office the Series ตอนที่ 2 การแก้ปัญหาคอมพิวเตอร์เบื้องต้น</h3>
+             </div>
+          </div>
+       </div>
+       <div class="row">
+       		<div class="col-md-12">
+        		<div class="col-md-4" style="float:left;">
+                	<a data-fancybox href="https://www.youtube.com/watch?v=sc7Mh8i8jI0&list=PL2MW50dbNlbYWFq9AycOoAvtQkR_zFNx2&index=5">
+      					<img src="images/km/ots_3.jpg" width="320" height="240"></a>
+                        	<h3>Office the Series ตอนที่ 3 ความแตกต่างระหว่างฮาร์ดแวร์กับซอฟต์แวร์</h3>
+     			</div>
+                <div class="col-md-4" style="float:left;">
+                	<a data-fancybox href="https://www.youtube.com/watch?v=K5kZy_S2lFs&list=PL2MW50dbNlbYWFq9AycOoAvtQkR_zFNx2&index=6">
+      					<img src="images/km/ots_4.png" width="320" height="240"></a>
+                        	<h3>Office the Series ตอนที่ 4 การป้องกันไวรัสคอมพิวเตอร์</h3>
+     			</div>
+               <div class="col-md-4" style="float:left;">
+                	<a data-fancybox href="https://www.youtube.com/watch?v=2JYLqpB-QA0&list=PL2MW50dbNlbYWFq9AycOoAvtQkR_zFNx2&index=7">
+      					<img src="images/km/ots_5.png" width="320" height="240"></a>
+                        	<h3>Office the Series ตอนที่ 5 ระบบต่างๆที่ใช้ภายใน อสส.(ระบบทั่วไป)</h3>	
+             </div>
+          </div>
+       </div>
+       <div class="row">
+       		<div class="col-md-12">
+        		<div class="col-md-4" style="float:left;">
+                	<a data-fancybox href="https://www.youtube.com/watch?v=ko4bsIRPx1I&index=8&list=PL2MW50dbNlbYWFq9AycOoAvtQkR_zFNx2">
+      					<img src="images/km/ots_6.png" width="320" height="240"></a>
+                        	<h3>Office the Series ตอนที่ 6 ระบบต่างๆที่ใช้ใน อสส. (เฉพาะทาง)</h3>
+     			</div>
+                <div class="col-md-4" style="float:left;">
+                	<a data-fancybox href="https://www.youtube.com/watch?v=_lhePrgym_M&index=9&list=PL2MW50dbNlbYWFq9AycOoAvtQkR_zFNx2">
+      					<img src="images/km/ots_7.jpg" width="320" height="240"></a>
+                        	<h3>Office the Series ตอนที่ 7 ประโยชน์และโทษของ Social Media</h3>
+     			</div>
+               <div class="col-md-4" style="float:left;">
+                	<a data-fancybox href="https://www.youtube.com/watch?v=rOi1qyuIPYs&index=10&list=PL2MW50dbNlbYWFq9AycOoAvtQkR_zFNx2">
+      					<img src="images/km/ots_8.jpg" width="320" height="240"></a>
+                        	<h3>Office the Series ตอนที่ 8 บทบาทและความสำคัญของเทคโนโลยีสารสนเทศ</h3>
+             </div>
+          </div>
+       </div>
+       <div class="row">
+       		<div class="col-md-12">
+        		<div class="col-md-4" style="float:left;">
+                	<a data-fancybox href="https://www.youtube.com/watch?v=uXJp9052pYo&list=PL2MW50dbNlbYWFq9AycOoAvtQkR_zFNx2&index=11">
+      					<img src="images/km/ots_9.jpg" width="320" height="240"></a>
+                        	<h3>Office the Series ตอนที่ 9 ถ้าไม่มีเทคโนโลยีสารสนเทศชีวิตจะเป็นอย่างไร</h3>
+     			</div>
+                <div class="col-md-4" style="float:left;">
+                	<a data-fancybox href="https://www.youtube.com/watch?v=g-r14ip2jk4&index=12&list=PL2MW50dbNlbYWFq9AycOoAvtQkR_zFNx2">
+      					<img src="images/km/ots_10.jpg" width="320" height="240"></a>
+                        	<h3>Office the Series ตอนที่ 10 การใช้ประโยชน์เทคโนโลยีสารสนเทศในองค์กร</h3>
+     			</div>
+               <div class="col-md-4" style="float:left;">
+          </div>
+       </div>
+   </div><!--- <div container---->
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,38 +4,38 @@
 	<meta charset="utf-8">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/main.css">
+</head>
 <body>
 	<div class="container">
-    	<div class="row">
-        	<div class="col-md-12">
-            	<img src="images/km/banner3.jpg" width="100%">
-				อยากให้รูปเหลือความสูงแค่ 1400px ความกว้าง 1600px
-            </div>
-	<!--	</div> ปิด div ผิดที่ -->
+		<div class="row">
 			<div class="col-md-12">
-				<div class="row">
-					<div class="col-md-2" style="float:left;">
-						<img src="images/km/active.jpg" width="200px" height="200px">
-					</div>       
-					<div class="col-md-2" style="float:left;">
-						<img src="images/km/st.jpg" width="200px" height="200px">
-					</div>
-					<div class="col-md-2" style="float:left;">
-						<img src="images/km/report.jpg" width="200px" height="200px">
-					</div>
-					<div class="col-md-2" style="float:left;">
-						<img src="images/km/low.jpg" width="200px" height="200px">
-					</div> 
-					<div class="col-md-2" style="float:left;">
-						<img src="images/km/animal.jpg" width="200px" height="200px">
-					</div>
-					<div class="col-md-2" style="float:left;">
-						<img src="images/km/computer.jpg" width="200px" height="200px">
-					</div>
-				</div> <!-- ปิด div ไม่ครบ-->
-            </div>
-        </div> <!-- ปิด class='row' ที่ตรงนี้ -->  
- 	</div>
+				<img src="images/km/banner.jpg" width="100%">
+					<!---อยากให้รูปเหลือความสูงแค่ 1400px ความกว้าง 1600px ---->
+ 			</div>
+<!--	</div> ปิด div ผิดที่ -->
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="col-md-2" style="float:left;">
+					<img src="images/km/active.jpg" width="200px" height="200px">
+				</div>       
+				<div class="col-md-2" style="float:left;">
+					<a href="Video.php"><img src="images/km/video.jpg" width="200px" height="200px"></a>
+				</div>
+				<div class="col-md-2" style="float:left;">
+					<img src="images/km/report.jpg" width="200px" height="200px">
+				</div>
+				<div class="col-md-2" style="float:left;">
+					<img src="images/km/law.jpg" width="200px" height="200px">
+				</div> 
+				<div class="col-md-2" style="float:left;">
+					<img src="images/km/animal.jpg" width="200px" height="200px">
+				</div>
+				<div class="col-md-2" style="float:left;">
+					<img src="images/km/computer.jpg" width="200px" height="200px">
+				</div>
+			</div> <!-- ปิด div ไม่ครบ-->
+		</div> <!-- ปิด class='row' ที่ตรงนี้ -->  
+	</div>
 </body>
-</head>
 </html>
